3cad8da87f24f29cc689cf789ef14c50c70b9e94
Amal070 <154653845+Amal070@users.noreply.github.com>
2024-12-13 09:43:30 +0530

    Add files via upload

--- queue.c
+++ queue.c
@@ -9,5 +9,20 @@ void main()
 {
 int choice,item;
 printf("Enter the size of queue:");
+scanf("%d",&maxsize);
+while(1)
+{
+printf("\n--- Queue Operations Menu ---\n");
+printf("1.Enqueue\n");
+printf("2.Dequeue\n");
+printf("3.Peek\n");
+printf("4.Check isfull\n");
+printf("5.Check isempty");
+printf("6.Exit\n");
+printf("Enter your choice:");
+scanf("%d",&choice);
+switch(choice)
+{
+case 1: printf("Enter the element
 
 
